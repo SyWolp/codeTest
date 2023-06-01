9c57661e3752e5430fe150f1820e318be65641aa
강신영 <98308234+SyWolp@users.noreply.github.com>
2023-06-01 16:05:26 +0900

    [unrated] Title: 달리기 경주, Time: 0.18 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.js
@@ -0,0 +1,18 @@
+      function solution(players, callings) {
+        const map = new Map();
+        const map2 = new Map();
+        for (let i = 0; i < players.length; i++) {
+          map.set(players[i], i);
+          map2.set(i, players[i]);
+        }
+        for (let j = 0; j < callings.length; j++) {
+          map.set(callings[j], map.get(callings[j]) - 1);
+          map.set(
+            map2.get(map.get(callings[j])),
+            map.get(map2.get(map.get(callings[j]))) + 1,
+          );
+          map2.set(map.get(callings[j]) + 1, map2.get(map.get(callings[j])));
+          map2.set(map.get(callings[j]), callings[j]);
+        }
+        return [...map].sort((a, b) => a[1] - b[1]).map((v) => v[0]);
+      }
