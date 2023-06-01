12ace4a084f916ac766f9c922fdfbec3b7f78930
강신영 <98308234+SyWolp@users.noreply.github.com>
2023-06-01 16:21:28 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 2.13 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
+++ 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
@@ -1,22 +1,4 @@
 function solution(arr) {
-  let i = 0;
-  let a = "";
-  let result = [];
-  while(i < arr.length) {
-    a = Math.min(...arr);
-    i++;
-  }
-  console.log(Math.min(a));
-  let j = 0;
-  while(j < arr.length) {
-    if(arr[j] != a) {
-      result.push(arr[j]);
-    }
-    j++;
-  }
-  if(result.length == 0) {
-    return [-1];
-  }else{
-    return result;
-  }
+    const a = arr.filter((vv, i) => i !== arr.indexOf(Math.min(...arr)), 1);
+    return a.length ? a : [-1]
 }
