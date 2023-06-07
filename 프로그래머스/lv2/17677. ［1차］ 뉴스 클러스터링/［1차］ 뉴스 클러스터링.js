583143210f6d1d76cb3e0af8111a1cd50cae78e4
강신영 <98308234+SyWolp@users.noreply.github.com>
2023-06-07 10:18:57 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 0.45 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.js
@@ -0,0 +1,34 @@
+function solution(str1, str2) {
+        const sumArr = [];
+        let [min, max] = [0, 0];
+        
+        const strings = (str) => {
+          const arr = [];
+          for (let i = 0; i < str.length; i++) {
+            if (/[a-zA-Z]/.test(str[i]) && /[a-zA-Z]/.test(str[i + 1]) && str[i + 1]) {
+              const s = str[i].toUpperCase() + str[i + 1].toUpperCase();
+              arr.push(s);
+              if (!sumArr.includes(s)) sumArr.push(s);
+            }
+          }
+          return arr;
+        };
+        const mapFn = (v) => {
+          const map = new Map();
+          const strArr = strings(v);
+          for (let i = 0; i < strArr.length; i++) {
+            if (map.has(strArr[i])) map.set(strArr[i], map.get(strArr[i]) + 1);
+            else map.set(strArr[i], 1);
+          }
+          return map;
+        };
+        const a1 = mapFn(str1);
+        const a2 = mapFn(str2);
+        for (let j = 0; j < sumArr.length; j++) {
+          const one = a1.has(sumArr[j]) ? a1.get(sumArr[j]) : 0;
+          const two = a2.has(sumArr[j]) ? a2.get(sumArr[j]) : 0;
+          min += Math.min(one, two);
+          max += Math.max(one, two);
+        }
+        return min ? parseInt((min / max) * 65536) : max ? 0 : 65536;
+      }
