aecfe18f8cbe2cc3c4eefae5d5a1aa18f88802a1
강신영 <98308234+SyWolp@users.noreply.github.com>
2023-06-09 12:01:58 +0900

    [unrated] Title: 숫자 카드 나누기, Time: 0.22 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/135807. 숫자 카드 나누기/숫자 카드 나누기.js
+++ 프로그래머스/unrated/135807. 숫자 카드 나누기/숫자 카드 나누기.js
@@ -1,38 +1,26 @@
 function solution(arrayA, arrayB) {
-        const aArr = [];
-        const cArr = [];
-        for (let i = 2; i <= arrayA[0]; i++) {
-          if (arrayA[0] % i === 0) aArr.push(i);
-        }
-        for (let i = 2; i <= arrayB[0]; i++) {
-          if (arrayB[0] % i === 0) cArr.push(i);
-        }
-        let result = -1;
-        let result2 = -1;
-        const bArr = aArr.sort((a, b) => b - a);
-        const dArr = cArr.sort((a, b) => b - a);
-        for (let j = 0; j < bArr.length; j++) {
-          if (result !== -1) break;
-          for (let k = 0; k < arrayA.length; k++) {
-            if (arrayA[k] % bArr[j] !== 0) break;
-            if (arrayB[k] % bArr[j] === 0) break;
-            if (k === arrayA.length - 1) {
-              result = bArr[j];
-              break;
+        const arrrrr = [];
+        const fn = (fArr, sArr) => {
+          const aArr = [];
+          for (let i = 2; i <= fArr[0]; i++) {
+            if (fArr[0] % i === 0) aArr.push(i);
+          }
+          let result = -1;
+          const bArr = aArr.sort((a, b) => b - a);
+          for (let j = 0; j < bArr.length; j++) {
+            if (result !== -1) break;
+            for (let k = 0; k < fArr.length; k++) {
+              if (sArr[k] % bArr[j] === 0) break;
+              if (fArr[k] % bArr[j] !== 0) break;
+              if (k === fArr.length - 1) {
+                result = bArr[j];
+                arrrrr.push(result);
+                break;
+              }
             }
           }
-        }
-        for (let j = 0; j < dArr.length; j++) {
-          if (result2 !== -1) break;
-          for (let k = 0; k < arrayA.length; k++) {
-            if (arrayA[k] % dArr[j] === 0) break;
-            if (arrayB[k] % dArr[j] !== 0) break;
-            if (k === arrayA.length - 1) {
-              result2 = dArr[j];
-              break;
-            }
-          }
-        }
-        const max = Math.max(result, result2);
-        return max === -1 ? 0 : max;
+        };
+        fn(arrayA, arrayB);
+        fn(arrayB, arrayA);
+        return arrrrr.length ? Math.max(...arrrrr) : 0;
       }
