3da960f01ad3416e92542d652feae96594825196
강신영 <98308234+SyWolp@users.noreply.github.com>
2023-10-12 13:51:56 +0900

    [level unrated] Title: 등수 매기기, Time: 0.16 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/120882. 등수 매기기/등수 매기기.js
+++ 프로그래머스/unrated/120882. 등수 매기기/등수 매기기.js
@@ -1,3 +1,1 @@
-  function solution(score) {
-    return score.map(v=>(v[0]+v[1])/2).map(v=> [...score.map(v=>(v[0]+v[1])/2)].sort((a,b) => b-a).indexOf(v)+1);
-}
+  const solution = score => score.map(v=>(v[0]+v[1])/2).map(v=> [...score.map(v=>(v[0]+v[1])/2)].sort((a,b) => b-a).indexOf(v)+1);
