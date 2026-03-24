3665b5d5c1401cf28beccd1b756141764a471ef3
강신영 <98308234+SyWolp@users.noreply.github.com>
2026-03-24 10:54:59 +0900

    [Bronze V] Title: 시험 성적, Time: 48 ms, Memory: 5476 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.cs
@@ -0,0 +1,21 @@
+using System;
+
+class Program 
+  {
+    static void Main(string[] args)
+      {
+        string? input = Console.ReadLine();
+        int? score = int.Parse(input);
+        if(score >= 90) {
+            Console.WriteLine('A');
+        } else if(score >= 80) {
+            Console.WriteLine("B");
+        } else if(score >= 70) {
+            Console.WriteLine('C');
+        } else if(score >= 60) {
+            Console.WriteLine('D');
+        } else {
+            Console.WriteLine('F');
+        }
+      }
+  }
